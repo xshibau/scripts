406b96b244d1efe264ff4b484966cd99f72ef67e
xshibau <ziugpro@gmail.com>
2026-06-09 16:49:41 +0700

    Update 36hubscript.lua

--- 36hubscript.lua
+++ 36hubscript.lua
@@ -1,5 +1,5 @@
 if game.GameId == 7326934954 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/scripts/refs/heads/main/99nightedit.lua"))()
-elseif game.GameId == 7939389895 then
+elseif game.GameId == 7939389895 or game.PlaceId == 7939389895 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/scripts/refs/heads/main/MadRoad.lua"))()
 end
